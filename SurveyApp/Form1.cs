3fe028233a16932093ab60591633e020440dd256
Alexis Ramos <lexy.b.quijano@gmail.com>
2025-11-24 12:53:36 -0800

    Added new round 2 question

--- SurveyApp/Form1.cs
+++ SurveyApp/Form1.cs
@@ -39,9 +39,7 @@ namespace SurveyApp{
                     timestamp TEXT,
                     name TEXT,
                     org TEXT,
-                    color TEXT,
-                    animal TEXT,
-                    prize INTEGER
+                    experience TEXT
                 );
             ";
 
